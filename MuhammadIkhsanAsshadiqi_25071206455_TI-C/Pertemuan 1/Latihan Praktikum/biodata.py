aabe413210798f1a072d51d69076bb18201ff473
muhammadikhsanasshadiqi16-rgb <muhammadikhsanasshadiqi16@gmail.com>
2026-02-12 21:56:22 +0700

    MuhammadIkhsanAsshadiqi_25071206455_TI-C

---
+++ MuhammadIkhsanAsshadiqi_25071206455_TI-C/Pertemuan 1/Latihan Praktikum/biodata.py
@@ -0,0 +1,10 @@
+nama = 'Muhammad Ikhsan Asshadiqi'
+kelas = 'TI-C'
+Nim = 25071206455
+Univ = 'UNIVERSITAS RIAU'
+Fakultas = 'Teknik'
+
+print ('Halo Semuanya, nama saya ' + nama, 'saya dari kelas', kelas)
+print ('dengan NIM ', str(Nim))
+print ('Saya berkuliah di ' + Univ)
+print ('di Fakultas ' + Fakultas)
---
+++ MuhammadIkhsanAsshadiqi_25071206455_TI-C/pertemuan2/latihan/soal5.py
@@ -0,0 +1,10 @@
+import math
+
+def jarak(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+x1, y1 = 1, 2
+x2, y2 = 4, 6
+
+hasil_jarak = jarak(x1, y1, x2, y2)
+print("Jarak =", hasil_jarak)
